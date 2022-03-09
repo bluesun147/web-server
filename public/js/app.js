d8d6d0fb226502643877feb4c4bdbf6fc19570b4
HaeChan Kim <bluesun147@naver.com>
2022-03-09 14:33:52 +0900

    search form

--- public/js/app.js
+++ public/js/app.js
@@ -2,22 +2,24 @@
 
 console.log('Client side js file is loaded!');
 
-// fetch('http://puzzle.mead.io/puzzle')
-// .then((response) => {
-//     response.json().then((data) => {
-//         console.log(data);
-//     })
-// })
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input'); // 입력창
 
-fetch('http://localhost:3000/weather?address=!')
-.then((response) => {
-    response.json().then((data) => { // json()은 json을 oject로 parse. JSON.paese()는 sync. 같은 역할. json()은 async
-        if (data.error) {
-            console.log(data.error);
-        } else {
-            console.log(`whole data :`, data);
-            console.log(`data.location : ${data.location}`);
-            console.log(`data.forecast : ${data.forecast}`);
-        }
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault(); // form은 자동으로 refresh.
+
+    const location = search.value; // 입력한 location
+
+    fetch(`http://localhost:3000/weather?address=${location}`)
+    .then((response) => {
+        response.json().then((data) => { // json()은 json을 oject로 parse. JSON.paese()는 sync. 같은 역할. json()은 async
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                console.log(`whole data :`, data);
+                console.log(`data.location : ${data.location}`);
+                console.log(`data.forecast : ${data.forecast}`);
+            }
+        })
     })
 })
