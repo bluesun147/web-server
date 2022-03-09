1df34d37c176826b88e3b027feb3529c307fa985
HaeChan Kim <bluesun147@naver.com>
2022-03-09 16:15:22 +0900

    UI

--- public/js/app.js
+++ public/js/app.js
@@ -5,17 +5,28 @@ console.log('Client side js file is loaded!');
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input'); // 입력창
 
+const messageOne = document.querySelector('#message-1'); // id
+const messageTwo = document.querySelector('#message-2');
+
+
+
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault(); // form은 자동으로 refresh.
 
     const location = search.value; // 입력한 location
+
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
 
     fetch(`http://localhost:3000/weather?address=${location}`)
     .then((response) => {
         response.json().then((data) => { // json()은 json을 oject로 parse. JSON.paese()는 sync. 같은 역할. json()은 async
             if (data.error) {
                 console.log(data.error);
+                messageOne.textContent = data.error;
             } else {
+                messageOne.textContent = `${data.location}`
+                messageTwo.textContent = `${data.forecast}`
                 console.log(`whole data :`, data);
                 console.log(`data.location : ${data.location}`);
                 console.log(`data.forecast : ${data.forecast}`);
